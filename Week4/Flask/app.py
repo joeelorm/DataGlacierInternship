a7b50881de0d3a5b71f39ffa2bac67e18b380fc8
joeelorm <joeelorm@outlook.com>
2021-09-12 21:39:49 +0200

    Update app.py

--- Week4/Flask/app.py
+++ Week4/Flask/app.py
@@ -1,9 +1,11 @@
 import numpy as np
 from flask import Flask, request, render_template
 import pickle
+import joblib
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+#model = pickle.load(open('model.pkl', 'rb'))
+model = joblib.load('predict_price.pkl')
 
 @app.route('/')
 def home():
@@ -11,13 +13,13 @@ def home():
 
 @app.route('/predict', methods = ['POST'])
 def predict():
-    int_features = [int(x) for x in request.form.values()]
+    int_features = [x for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text = 'Place Holder text here'.format(output))
+    return render_template('index.html', prediction_text = 'The price of the BMW is: {} dollars'.format(output))
 
 if __name__ == "__main__":
-    app.run(port = 5060, debug = True)
+    app.run(port = 5000, debug = True)
